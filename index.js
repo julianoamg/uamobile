6adfc77a4d4b005a0cbf6ec43a22b4609775467f
juliano <julianoamg.dev@gmail.com>
2023-09-17 22:14:12 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import UserAgent from 'user-agents';
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+	const userAgent = new UserAgent({ deviceCategory: 'mobile' })
+  	res.json(userAgent.toString())
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
